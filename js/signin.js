c7ef1ee65434969e80438b5b1bbbf35a1d2383af
nhuhung19 <nhuhung19@gmail.com>
2020-03-15 19:44:12 +0700

    make feature insert img url

--- js/signin.js
+++ js/signin.js
@@ -1,9 +1,13 @@
 let userInput = document.getElementById('inputUser')
+const signInButton = document.getElementById("signIn");
 
-const signIn = () => {
+const signIn = (e) => {
+    e.preventDefault()
     let user = {
         userName: userInput.value
     }
     localStorage.setItem('data', JSON.stringify(user))
-    window.open("main.html")
+    window.location.replace("/main.html")
 }
+
+signInButton.addEventListener("click", signIn)
